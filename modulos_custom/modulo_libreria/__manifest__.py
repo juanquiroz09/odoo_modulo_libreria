b39fe0bb3f7cef6f36b222910ba5772609eaba84
Juan Quiroz <juan.quiroz1009@gmail.com>
2024-02-09 01:59:45 -0500

    first-commit

---
+++ modulos_custom/modulo_libreria/__manifest__.py
@@ -0,0 +1,10 @@
+# -- coding: utf-8 -*-
+{
+    'name': "Modulo para Libros",
+    'summary': """Modulo para listar libros con sus autores""",
+    'author': "Juan Quiroz",
+    'category': "General",
+    'version': '1.0.0',
+    'depends': [],
+    'data': [],
+}
